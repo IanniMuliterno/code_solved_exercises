70f796fa0f757c81d74233e62e36a331abf08fd4
Ían Muliterno <iannimuliterno@gmail.com>
2025-03-05 20:33:49 -0300

    Create longest_substr_most_2dist.py
    longest substring with at most two distinct characters

---
+++ basic_python/longest_substr_most_2dist.py
@@ -0,0 +1,20 @@
+#Problem: Longest Substring with At Most Two Distinct Characters
+#Description:
+#Given a string s, return the length of the longest substring that contains at most two distinct characters.
+
+"""
+e.g.
+
+s = "eceba"
+length_of_longest_substring_two_distinct(s)
+>>> 3
+
+ece
+
+s = "ccaabbb"
+length_of_longest_substring_two_distinct(s)
+>>> 5 
+
+aabbb
+
+"""
