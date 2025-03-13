14e01f880a9d3de1dcf09ded1aa7960f452bd536
Ían Muliterno <iannimuliterno@gmail.com>
2025-03-12 23:43:05 -0300

    Create top3_salaries.sql
    ongoing solution

---
+++ SQL/top3_salaries.sql
@@ -0,0 +1,12 @@
+-- credits to leet code
+--A company's executives are interested in seeing who earns the most money in each of the company's departments. A high earner in a department is an employee who has a salary
+--in the top three unique salaries for that department.
+
+--Write a solution to find the employees who are high earners in each of the departments.
+
+--Return the result table in any order.
+
+select d.name as Department, 
+e.name as Employee, salary ,dense_rank() over (partition by d.name order by salary desc) as rank_salary
+from department d
+left join employee e on d.id = e.departmentId
